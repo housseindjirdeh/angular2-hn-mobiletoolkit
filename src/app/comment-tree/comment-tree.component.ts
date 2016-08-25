98300172e3a3f7c0423a4f580128ea5e32ce6809
hdjirdeh <h.djirdeh@mail.utoronto.ca>
2016-08-25 12:31:55 -0400

    added load more button

--- src/app/comment-tree/comment-tree.component.ts
+++ src/app/comment-tree/comment-tree.component.ts
@@ -9,9 +9,12 @@ import { HackerNewsAPIService } from '../services/hackernews-api.service';
   selector: 'app-comment-tree',
   template: `
     <ul class="comment-list">
-      <li *ngFor="let itemID of itemKids">
+      <li *ngFor="let itemID of itemKids | slice:0:indexFrom">
         <app-comment commentID="{{ itemID }}"></app-comment>
       </li>
+      <div *ngIf="indexFrom < itemKids.length" class="button-block">
+        <button (click)="loadMore()" class="button">Load More</button>
+      </div>
     </ul>
   `,
   styles: [`
@@ -23,16 +26,46 @@ import { HackerNewsAPIService } from '../services/hackernews-api.service';
     li {
       display: list-item;
     }
+
+    .button-block {
+      text-align: center;
+    }
+
+    .button {
+      display: inline-block;
+      border-radius: 4px;
+      background-color: #B13138;
+      border: none;
+      color: #FFFFFF;
+      text-align: center;
+      font-size: 16px;
+      padding: 10px;
+      width: 130px;
+      transition: all 0.5s;
+      cursor: pointer;
+      margin: 35px 0;
+      vertical-align: middle;
+    }
+
+    .button:hover {
+      background-color: #b8454b;
+    }
   `],
   directives: [CommentComponent]
 })
 
 export default class CommentTreeComponent implements OnInit {
   @Input() itemKids: string;
+  indexFrom: number;
 
   constructor(private _hackerNewsAPIService: HackerNewsAPIService) {}
 
   ngOnInit() {
     this.itemKids = JSON.parse("[" + this.itemKids + "]");
+    this.indexFrom = 8;
+  }
+
+  loadMore() {
+    this.indexFrom += 8;
   }
 }
